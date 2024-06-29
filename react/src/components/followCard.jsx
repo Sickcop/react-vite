e978f236eacf351dc5cd0dab4ab6346622ec20a0
Sickcop <juan,hurtado2@ucp.edu.co>
2024-06-29 12:59:08 -0500

    addee-button

--- react/src/components/followCard.jsx
+++ react/src/components/followCard.jsx
@@ -1,19 +1,39 @@
 import './followCard.styles.css'
+import { useState } from 'react'
 
-export function FollowCard({userName, isfollow, name}) {
 
-  const imgSrc = `https://unavatar.io/${userName}`
+export function FollowCard({ userName, name }) {
+
+  const [isFollow, setFollow] = useState(false)
+  const [hover, setHover] = useState(false)
+
+  const toggleFollow = ()=>{
+    setFollow(!isFollow)
+  }
+
+  const hovering = () => {
+    setHover(true)
+  }
+
+  const leaveHovering = () => {
+    setHover(false)
+  }
+
+  const text = isFollow? hover? "dejar de seguir" : "siguiendo" : "seguir"
+  const btnClass = isFollow? "btn-follow" : "btn-unfollow"
+
+  const imgSrc = `https://unavatar.io/${ userName }`
 
   return (
     <article className='general-container'>
       <header className="info-container">
-      <img alt="avatar" src={imgSrc} className="avatar"/>
+      <img alt="avatar" src={ imgSrc } className="avatar"/>
         <div>    
-          <h1>{name}</h1>
-          <p>@{userName}</p>
+          <h1>{ name }</h1>
+          <p>@{ userName }</p>
         </div>
       </header>
-      <button className="btn-follow">Seguir</button>
+      <button onMouseLeave={leaveHovering} onMouseEnter={hovering} onClick={ toggleFollow } className={ btnClass }>{ text }</button>
     </article>
   )
 }
